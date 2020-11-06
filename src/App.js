bd5c93d2158594f8a22da2e2bdd737abc30909f8
koiduka tatsuki <>
2020-11-06 15:45:46 +0900

    learn component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class App extends Component{
-  render(){
-    return (
-     <React.Fragment> 
-      <label htmlFor="bar">bar</label>
-      <input type="text" onClick={()=>{console.log("I clicked")}} />
-     </ React.Fragment> 
-    );
-  }
-}
+// クラスコンポーネント
+// class App extends Component{
+//   render(){
+//     return (
+//      <React.Fragment> 
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onClick={()=>{console.log("I clicked")}} />
+//      </ React.Fragment> 
+//     );
+//   }
+// }
 
+const App = ()=>{
+  return (
+  <div>
+   <p>hi</p>
+   <p>hi</p>
+   <p>hi</p>
+   <p>hi</p>
+  </div>
+  )}
 export default App;
