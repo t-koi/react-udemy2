319c447b61cc958013a91dedc4d51fe440932af4
koiduka tatsuki <>
2020-11-06 16:54:08 +0900

    create props

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 // import React, { Component } from 'react';
+import React from 'react';
 
-// クラスコンポーネント
-// class App extends Component{
-//   render(){
-//     return (
-//      <React.Fragment> 
-//       <label htmlFor="bar">bar</label>
-//       <input type="text" onClick={()=>{console.log("I clicked")}} />
-//      </ React.Fragment> 
-//     );
-//   }
-// }
+const App = () =>{
+  const profiles =[
+    {name: "taro" ,old: 10},
+    {name: "naoko", old: 5},
+    {name: "kikko"}
+  ]
 
-const App = ()=>{
+  return(
+    <div>
+      {
+        profiles.map((profile, index)=>{
+          return  <User name={profile.name} old={profile.old} key={index}/>
+        })
+      }
+    </div>
+  )
+}
+
+const User = (props)=>{
   return (
   <div>
-   <p>hi</p>
-   <p>hi</p>
-   <p>hi</p>
-   <p>hi</p>
+    HI i am {props.name}, {props.old}old
   </div>
   )}
+
+  User.defaultProps = {
+    old: 1
+  }
+
 export default App;
